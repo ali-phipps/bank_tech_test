37518f4495279097b8ea21977a6ccbd12181fc6b
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-12-17 16:06:41 +0000

    removed some early account tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,24 +2,12 @@ require 'account'
 require 'timecop'
 
 describe Account do
-  subject(:account) { Account.new(name: 'Alistair Phipps'}
+  subject(:account) { Account.new(name: 'Alistair Phipps')}
 
   before(:each) do
     Timecop.freeze(Time.local(2019, 12, 17))
   end
-
-  describe '#initialise account' do
-    it 'new account is created for a customer and
-    has an opening balance of zero' do
-      account = Account.new(name: 'Alistair Phipps')
-      expect(account.balance).to eq 0
-    end
-
-    it 'new account is created for a customer and has user specified balance' do
-      expect(account.balance).to eq 1000
-    end
-  end
-
+  
   describe '#deposit' do
     it 'make a single deposit' do
       transaction = account.deposit(1000)
