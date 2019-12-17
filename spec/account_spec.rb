f203acff1cada4d774c83458722cf76cab17c8e7
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-12-17 11:01:46 +0000

    first initialise test passing

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,9 +2,12 @@ require 'account'
 
 describe Account do
 
+  before(:all) do
+    account = Account.new(name: "Alistair Phipps")
+  end
+
   describe "initialise account" do
     it "new account is created for a customer and has an opening balance of zero" do
-      account = Account.new(name: "Alistair Phipps")
 
       expect(account.balance).to eq 0
     end
