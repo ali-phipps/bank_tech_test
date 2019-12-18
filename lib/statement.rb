4daab106572e1f810c9a4e6b2f30179fc980d45a
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-12-18 09:37:54 +0000

    small refactor on Statement.print

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,21 +2,20 @@ class Statement
 
   def self.print(transactions)
     balance = 0
-    header = "date || credit || debit || balance\n"
-    lines = header
+    lines = "date || credit || debit || balance\n"
 
     transactions.history.each do |transaction|
-      line = transaction.date + " || "
       if transaction.type == "credit"
         balance += transaction.amount
-        line += transaction.amount.to_s + ' || || ' + balance.to_s + "\n"
+        lines += transaction.date + " || " + transaction.amount.to_s +
+                  ' || || ' + balance.to_s + "\n"
       else
         balance -= transaction.amount
-        line += '|| ' + transaction.amount.to_s + ' || ' + balance.to_s  + "\n"
+        lines += transaction.date + " || " + '|| ' + transaction.amount.to_s +
+         ' || ' + balance.to_s  + "\n"
       end
-
-      lines += line
     end
-    return lines
+    lines
   end
+
 end
